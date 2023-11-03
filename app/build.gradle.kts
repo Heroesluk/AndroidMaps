24c7ff1dbe4c993699aac00fb29368c68b2706a2
242209 <242209@edu.p.lodz.pl>
2023-11-03 18:30:19 +0100

    works

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.example.myapplication"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.myapplication"
@@ -51,20 +51,17 @@ android {
 }
 
 dependencies {
-
-    implementation("io.kotlintest:kotlintest-runner-junit5:3.1.9")
+    implementation("com.google.guava:guava:27.0.1-android")
+    implementation("androidx.navigation:navigation-compose:2.5.3")
     testImplementation("org.junit.jupiter:junit-jupiter:5.8.0")
-
-    implementation("androidx.navigation:navigation-compose:2.7.4")
     implementation("androidx.compose.material3:material3")
-    implementation("androidx.core:core-ktx:1.9.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
-    implementation("androidx.activity:activity-compose:1.7.0")
+    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
+    implementation("androidx.activity:activity-compose:1.8.0")
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.compose.material3:material3")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
